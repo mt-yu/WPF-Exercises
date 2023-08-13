cebb1340e39c2f6b04103c38b177a62212cca505
mt-yu <30967271+mt-yu@users.noreply.github.com>
2023-08-13 12:53:09 +0800

    WPF框架 MvvmLight

---
+++ MyToDo.Mvvmlight/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace MyToDo.Mvvmlight
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            Name = "MtTest";
+            ShowCommand = new RelayCommand<string>(Show);
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; RaisePropertyChanged(); }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; RaisePropertyChanged(); }
+        }
+
+        public RelayCommand<string> ShowCommand { get; set; }
+
+
+        public void Show(string content)
+        {
+            Name = "点击了按钮！";
+            Title = "我是标题";
+            //MessageBox.Show(content);
+
+            // 给 "Token1" 的地址发送一个 string 类型的值 content
+            Messenger.Default.Send(content, "Token1");
+        }
+    }
+}
---
+++ MyToDo.Mvvmlight/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using GalaSoft.MvvmLight.Messaging;
+namespace MyToDo.Mvvmlight
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+
+            // 注册一个接受string 类型参数的消息，地址是"Token1"
+            Messenger.Default.Register<string>(this, "Token1", Show);
+        }
+
+        private void Show(string obj)
+        {
+            MessageBox.Show(obj);
+        }
+    }
+}
