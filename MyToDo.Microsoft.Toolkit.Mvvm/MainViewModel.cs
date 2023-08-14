deda711bc7f015378187c87df079090f7224125e
mt-yu <30967271+mt-yu@users.noreply.github.com>
2023-08-14 22:25:04 +0800

    WPF 框架 Microsoft.ToolKit.Mvvm(不维护)

---
+++ MyToDo.Microsoft.Toolkit.Mvvm/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Microsoft.Toolkit.Mvvm.Messaging;
+
+namespace MyToDo.Microsoft.Toolkit.Mvvm
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            Name = "MtTest";
+            ShowCommand = new RelayCommand<string>(Show);
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged(); }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; OnPropertyChanged(); }
+        }
+
+        public RelayCommand<string> ShowCommand { get; set; }
+
+
+        public void Show(string content)
+        {
+            Name = "点击了按钮！";
+            Title = "我是标题";
+            //MessageBox.Show(content);
+
+            // 给 "Token1" 的地址发送一个 string 类型的值 content
+            WeakReferenceMessenger.Default.Send(content, "Token1");
+        }
+    }
+}
---
+++ MyToDo.Microsoft.Toolkit.Mvvm/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Toolkit.Mvvm.Messaging;
+
+namespace MyToDo.Microsoft.Toolkit.Mvvm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+
+            // 注册一个接受string 类型参数的消息，地址是"Token1"
+            WeakReferenceMessenger.Default.Register<string, string>(this, "Token1", Show);
+        }
+
+        private void Show(object recipient, string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
