5ba06c745fb0ccadcc8cb77211cdcd8b6831cdf6
mt-yu <30967271+mt-yu@users.noreply.github.com>
2023-08-15 23:32:01 +0800

    手动添加 Prism.DryIoc 框架

---
+++ MyToDo.UsePrism/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System.Windows;
+
+namespace MyToDo.UsePrism
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
