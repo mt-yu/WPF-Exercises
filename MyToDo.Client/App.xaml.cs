791e1e350720833eb1cd5c5f3fee9fc168215803
mt-yu <30967271+mt-yu@users.noreply.github.com>
2023-08-16 21:59:01 +0800

    创建项目结构

---
+++ MyToDo.Client/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MyToDo.Client.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System.Windows;
+
+namespace MyToDo.Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+1. 创建wpf 项目
+2. 安装 prism.dryioc mvvm 以及 ioc 框架
+	2.1 继承 PrismApplication
+3. 安装 ui 框架 MaterialDesignThemes
+	3.1 打开 MaterialDesign 的  github wiki 查看 如何操作 https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit/wiki/Getting-Started
+	3.2 按照说明设计即可
+4. 创建 web.api 项目
+	4.1 关闭 配置Https 
