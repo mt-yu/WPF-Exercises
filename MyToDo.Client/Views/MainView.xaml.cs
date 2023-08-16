b122bce5efb9ae93387b4387deb1587e9515aef4
mt-yu <30967271+mt-yu@users.noreply.github.com>
2023-08-16 23:27:22 +0800

    设计首页导航条

--- MyToDo.Client/Views/MainView.xaml.cs
+++ MyToDo.Client/Views/MainView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,45 @@ namespace MyToDo.Client.Views
         public MainView()
         {
             InitializeComponent();
+            
+
+            btnMin.Click += (s, e) => { this.WindowState = WindowState.Minimized; };
+            btnMax.Click += (s, e) => 
+            {
+                var btn = (ContentControl)FindName("btnMax");
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    ((PackIcon)(btn).Content).Kind = PackIconKind.WindowMaximize;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    ((PackIcon)(btn).Content).Kind = PackIconKind.WindowRestore;
+                }
+            };
+            btnClose.Click += (s, e) => { this.Close(); };
+            cloroZone.MouseMove += (s, e) =>
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    this.DragMove();
+                }
+            };
+            cloroZone.MouseDoubleClick += (s, e) =>
+            {
+                var btn = (ContentControl)FindName("btnMax");
+                if (this.WindowState == WindowState.Normal)
+                {
+                    this.WindowState = WindowState.Maximized;
+                    ((PackIcon)(btn).Content).Kind = PackIconKind.WindowRestore;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Normal;
+                    ((PackIcon)(btn).Content).Kind = PackIconKind.WindowMaximize;
+                }
+            };
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
+一. 创建项目结构
 1. 创建wpf 项目
 2. 安装 prism.dryioc mvvm 以及 ioc 框架
 	2.1 继承 PrismApplication
@@ -6,3 +7,5 @@
 	3.2 按照说明设计即可
 4. 创建 web.api 项目
 	4.1 关闭 配置Https 
+
+二.
