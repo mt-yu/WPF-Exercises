84acc0df4ee41b9806494a098b6156385ad492ff
mt-yu <30967271+mt-yu@users.noreply.github.com>
2023-08-17 23:32:42 +0800

    MyToDo 设计首页-2

---
+++ MyToDo.Client/Common/Models/BaseDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.Client.Common.Models
+{
+    public class BaseDto
+    {
+		private int id;
+        private DateTime createDate;
+        private DateTime updateDate;
+
+        public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		public DateTime CreateDate
+		{
+			get { return createDate; }
+			set { createDate = value; }
+		}
+
+		public DateTime UpdateDate
+		{
+			get { return updateDate; }
+			set { updateDate = value; }
+		}
+
+	}
+}
--- MyToDo.Client/ViewModels/IndexViewModel.cs
+++ MyToDo.Client/ViewModels/IndexViewModel.cs
@@ -12,16 +12,31 @@ namespace MyToDo.Client.ViewModels
     public class IndexViewModel : BindableBase
     {
         public ObservableCollection<TaskBar> taskBars;
+        public ObservableCollection<ToDoDto> toDoDtos;
+        public ObservableCollection<MemoDto> memoDtos;
 
         public IndexViewModel()
         {
             CreateTaskBars();
+            CreateTestData();
         }
 
         public ObservableCollection<TaskBar> TaskBars
         {
             get { return taskBars; }
             set { taskBars = value;  RaisePropertyChanged(); }
+        }
+
+        public ObservableCollection<ToDoDto> ToDoDtos
+        {
+            get { return toDoDtos; }
+            set { toDoDtos = value; RaisePropertyChanged(); }
+        }
+
+        public ObservableCollection<MemoDto>  MemoDtos
+        {
+            get { return memoDtos; }
+            set { memoDtos = value; RaisePropertyChanged(); }
         }
 
         void CreateTaskBars()
@@ -31,6 +46,17 @@ namespace MyToDo.Client.ViewModels
             TaskBars.Add(new TaskBar() { Icon = "ClockCheckOutline", Title = "已完成", Content = "9", Color = "#0eb138", Target = "" });
             TaskBars.Add(new TaskBar() { Icon = "ChartLineVariant", Title = "完成比例", Content = "100%", Color = "#00b4dd", Target = "" });
             TaskBars.Add(new TaskBar() { Icon = "PlaylistStar", Title = "备忘录 ", Content = "19", Color = "#ff9f00", Target = "" });
+        }
+
+        void CreateTestData()
+        {
+            ToDoDtos = new ObservableCollection<ToDoDto>();
+            MemoDtos = new ObservableCollection<MemoDto>();
+            for (int i = 0; i < 10; i++)
+            {
+                ToDoDtos.Add(new ToDoDto() { Title=@$"{i}-todo", Content= @$"{i}-todo..." });
+                MemoDtos.Add(new MemoDto() { Title = @$"{i}-memo", Content = @$"{i}-memo..." });
+            }
         }
     }
 }
