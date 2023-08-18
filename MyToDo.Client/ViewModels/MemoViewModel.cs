6920147e21970030361aef04e78c2e71e2b75b1b
mt-yu <30967271+mt-yu@users.noreply.github.com>
2023-08-18 09:43:32 +0800

    MyToDo 设计备忘录界面

--- MyToDo.Client/ViewModels/MemoViewModel.cs
+++ MyToDo.Client/ViewModels/MemoViewModel.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using MyToDo.Client.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyToDo.Client.ViewModels
 {
-    internal class MemoViewModel
+    public class MemoViewModel : BindableBase
     {
+        private ObservableCollection<MemoDto> memoDots;
+        private bool isRightDrawerOpen;
+
+        public MemoViewModel()
+        {
+            MemoDtos = new ObservableCollection<MemoDto>();
+            AddCommand = new DelegateCommand(Add);
+            CreateTestData();
+        }
+
+        private void Add()
+        {
+            IsRightDrawerOpen = true;
+        }
+
+        public DelegateCommand AddCommand { get; private set; }
+
+        public ObservableCollection<MemoDto> MemoDtos
+        {
+            get { return memoDots; }
+            set { memoDots = value; RaisePropertyChanged(); }
+        }
+
+        /// <summary>
+        /// 右侧编辑窗口是否展开
+        /// </summary>
+        public bool IsRightDrawerOpen
+        {
+            get { return isRightDrawerOpen; }
+            set { isRightDrawerOpen = value; RaisePropertyChanged(); }
+        }
+
+        private void CreateTestData()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                MemoDtos.Add(new MemoDto { Title = $"{i}-memo", Content = $"{i}-memo..." });
+            }
+        }
     }
 }
