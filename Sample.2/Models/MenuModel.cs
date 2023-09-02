4a8eda0c610f980be487f125db011a02c54a9a9c
mt-yu <30967271+mt-yu@users.noreply.github.com>
2023-09-02 15:08:34 +0800

    界面练习 Sample.2 添加左侧菜单

---
+++ Sample.2/Models/MenuModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample._2.Models
+{
+    public class MenuModel
+    {
+        public string Icon { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Sample.2/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Sample._2.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample._2.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            Title = "Sample 2";
+            CreateMenu();
+        }
+
+        private void CreateMenu()
+        {
+            Menus = new ObservableCollection<MenuModel>()
+            {
+                new MenuModel(){ Icon = "\xe608", Name = "Live scores" },
+                new MenuModel(){ Icon = "\xe620", Name = "Series" },
+                new MenuModel(){ Icon = "\xe622", Name = "Teams" },
+                new MenuModel(){ Icon = "\xe603", Name = "Features" },
+                new MenuModel(){ Icon = "\xe51c", Name = "Videos" },
+                new MenuModel(){ Icon = "\xe790", Name = "Stats" },
+                new MenuModel(){ Icon = "\xe672", Name = "World Cup 2019" },
+            };
+        }
+
+        public string Title { get; set; }
+
+        private ObservableCollection<MenuModel> meuns;
+
+        public ObservableCollection<MenuModel> Menus
+        {
+            get { return meuns; }
+            set { meuns = value; OnPropertyChanged(); }
+        }
+
+    }
+}
